d12cce8f4e2204e14dff9b6b0a3c76a69ba0a27c
Andrii Veremii <dashuk10@gmail.com>
2025-06-20 12:57:17 +0200

    Add dashuk_HW/settings.py to Django project

--- django/dashuk_HW/settings.py
+++ django/dashuk_HW/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'dashuk_HW.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+		'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+		'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+		'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+		'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+	}
 }
 
 
