773c7ff76ce85065c09d1def4762de5a7f16c845
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2025-07-14 21:45:46 +0200

    add

--- lesson-5/modules/ecr/ecr.tf
+++ lesson-5/modules/ecr/ecr.tf
@@ -6,7 +6,31 @@ resource "aws_ecr_repository" "this" {
   }
 
   tags = {
-    Name = "Dashuk ECR Repo"
+    Name = var.repository_name
     Project = "Lesson 5"
   }
+}
+
+resource "aws_ecr_repository_policy" "this" {
+  repository = aws_ecr_repository.this.name
+
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Sid       = "AllowPushPull"
+        Effect    = "Allow"
+        Principal = "*"
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+      }
+    ]
+  })
 }
--- lesson-5/modules/ecr/outputs.tf
+++ lesson-5/modules/ecr/outputs.tf
@@ -1,4 +1,6 @@
-output "ecr_url" {
+output "repository_url" {
+  description = "URL of the ECR repository"
   value = aws_ecr_repository.this.repository_url
 }
+
 
